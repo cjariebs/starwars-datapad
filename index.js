1066751d4ddfa78d369da17155d0c137dd452867
Christopher Riebs <chris@riebs.io>
2018-11-20 16:37:50 -0800

    js skeleton from wireframe/stories

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 'use strict';
 $(() => {
 
+    const _playerInfo = {
+	name: "Makaec Aurai",
+	credits: 100000,
+	inventory: [],
+	location: "Tattooine",
+	currentShip: "Slave I",
+    };
     
+    function renderPlayerInfo() {
+	renderPlayerCard();
+	renderPlayerLocation();
+    }
 
+    function renderPlayerCard() {
+	const card = $('.playerCard');
+
+    }
+
+    function renderPlayerLocation() {
+	const loc = $('.playerLocation');
+
+    }
+
+    function renderGameContent() {
+	const content = $('.gameContent');
+
+    }
+
+    function render() {
+	renderPlayerInfo();
+	renderGameContent();
+    }
+
+    // ------------------------------------------------------------------------
+
+    function getPlanets() {
+
+    }
+
+    function getShips() {
+
+    }
+
+    function buyShip() {
+
+    }
+
+    function showShip() {
+
+    }
+
+    function showPlanet() {
+
+    }
+
+    function flyToPlanet() {
+
+    }
 });
 
