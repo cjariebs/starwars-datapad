4934382e2bce8d5062c5e2bed7df19a8e432253d
Christopher Riebs <chris@riebs.io>
2018-11-26 18:40:05 -0800

    implement router, implement getPlanet by search or id

--- index.js
+++ index.js
@@ -11,12 +11,103 @@ function swapiGet(identifier) {
 	});
 }
 
+function uriToState(uri) {
+    console.log(`uriToState: ${uri}`);
+    // remove # from beginning of uri
+    uri = uri.replace(/^#/,'');
+    const split = uri.split('/');
+    const locator = split.shift();
+    const params = split;
+
+    /*
+     * 'planet/Alderaan' -> search
+     * 'planets/Alderaan' -> search
+     * 'planet/2' -> id
+     * 'planets/2' -> id
+     * 'planets?page=2' -> list
+     * 'planet?page=2' -> list
+     */
+
+    const validResources = ['', 'planet', 'planets', 'starships', 'starship', 'species', 'people', 'person', 'vehicles', 'vehicle'];
+
+    console.log(`locator ${locator}`);
+    console.log(`params ${params}`);
+
+    if (validResources.indexOf(locator) == -1) {
+	// error case, locator is not valid
+	return;
+    }
+
+    let query = uri.split('?');
+    if (query.length > 1) {
+	query = query.pop();
+	console.log(`query ${query}`);
+    } else {
+	query = '';
+    }
+    
+    switch (locator) {
+	case '':
+	    // no args - welcome page
+	    renderWelcomePage();
+	    break;
+
+	case 'planets':
+	case 'planet':
+	    // '#planets' no args - list mode
+	    if (params == '') {
+		let page = 1;
+		if (query !== '') {
+		    page = (query.split('='))[1];
+		}
+
+		console.log('list mode');
+		renderPlanetList(page);
+	    } else {
+		renderPlanet(params);
+	    } 
+	    break;
+/*	
+	case 'starships':
+	    break;
+
+	case 'starship':
+	    break;
+
+	case 'species':
+	    break;
+
+	case 'people':
+	    break;
+
+	case 'person':
+	    break;
+	
+	case 'vehicles':
+	    break;
+
+	case 'vehicle':
+	    break;
+*/
+    }
+}
+
 function getPlanets(page=1) {
     return swapiGet(`planets/?page=${page}`);
 }
 
 function getPlanet(planetId) {
-    return swapiGet(`planets/${planetId}`);
+    // if params is NaN then use search mode
+    if (Number.isNaN(Number.parseInt(planetId))) {
+	console.log('search mode');
+	return swapiGet(`planets/?search=${planetId}`)
+        .then(json => {
+            return json.results[0];
+	});
+    } else {
+	console.log('id mode');
+        return swapiGet(`planets/${planetId}`);
+    }
 }
 
 function readyMainPane() {
@@ -25,37 +116,48 @@ function readyMainPane() {
     return mainPane;
 }
 
-function renderPlanetList() {
+function renderWelcomePage() {
+    console.log('rendering welcome page');
     const pane = readyMainPane();
 
-    getPlanets()
+    pane.html('<h1>Welcome</h1>');
+}
+
+function renderPlanetList(page=1) {
+    console.log(`rendering planet list at page ${page}`);
+    const pane = readyMainPane();
+    
+    location.hash = `#planets/?page=${page}`;
+
+    getPlanets(page)
     .then(json => {
 	const planets = json.results;
 	console.log(planets);
 
 	let html = '<ul>';
 	for (let i=0; i < planets.length; i++) {
-	    html += `<li><a href="#planet">${planets[i].name}</a></li>`;
+	    html += `<li><a href="#planet/${planets[i].name}">${planets[i].name}</a></li>`;
 	}
 	html += '</ul>';
 
-	// TODO: parse URL to get current page and to reconstruct local uri
 	if (json.prev) {
-	    html += `<a href="${json.prev}">Previous</a>`;
+	    html += `<a href="#planets/?page=${page-1}">Previous</a>`;
 	}
 
-	html += ` Showing 10 of ${json.count} results `;
+	html += ` Showing ${1+((page-1)*10)}-${(1+((page-1)*10))+(planets.length-1)} of ${json.count} results `;
 
 	if (json.next) {
-	    html += `<a href="${json.next}">Next</a>`;
+	    html += `<a href="#planets/?page=${page+1}">Next</a>`;
 	}
 
 	pane.html(html);
     });
 }
 
-function renderPlanet(planetId=1) {
+function renderPlanet(planetId) {
     const pane = readyMainPane();
+
+    location.hash = `#planets/${planetId}`;
 
     getPlanet(planetId)
     .then(planet => {
@@ -74,8 +176,16 @@ function getDigestibleResidents(planet) {
     return '<a href="#">Luke Skywalker</a>, <a href="#">C-3P0</a>, and <a href="#">Darth Vader</a>';
 }
 
+function setupUriHandler() {
+    $(window).on('hashchange', event => {
+	uriToState(location.hash);	
+    });
+}
+
 function init() {
-    renderPlanetList();
+    setupUriHandler();
+    uriToState(location.hash);
 }
 
 $(init);
+
